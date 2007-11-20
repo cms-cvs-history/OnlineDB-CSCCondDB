2ab7bd0ea8b484a09c1671a64edcf446217ad55b
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2007-11-20 13:07:39 +0000

    new for mapping DB

--- test/exec.sh
+++ test/exec.sh
@@ -3,6 +3,8 @@
 setenv TNS_ADMIN /afs/cern.ch/project/oracle/admin
 setenv CONDBON_AUTH_USER cms_csc
 setenv CONDBON_AUTH_PASSWORD csc_online_cond
-setenv CSCMAP_AUTH_USER cms_emu_cern
-setenv CSCMAP_AUTH_PASSWORD emu_test_cern
+#setenv CSCMAP_AUTH_USER cms_emu_cern
+#setenv CSCMAP_AUTH_PASSWORD emu_test_cern
+setenv CSCMAP_AUTH_USER cms_csc_config
+setenv CSCMAP_AUTH_PASSWORD config_emu_pass
 setenv CORAL_AUTH_PATH /afs/cern.ch/cms/DB/conddb
