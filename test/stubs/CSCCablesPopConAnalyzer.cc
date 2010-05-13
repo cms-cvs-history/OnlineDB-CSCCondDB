ca3108ed58f4f48f03b67b6adf4cfdddfb37b9bf
Oana Vickey Boeriu <sha1-a5ceae9af4b986cf7f52071a8ae913361a62b84e@cern.ch>
2010-05-13 20:44:56 +0000

    removed

--- test/stubs/CSCCablesPopConAnalyzer.cc
+++
@@ -1,7 +0,0 @@
-#include "CondCore/PopCon/interface/PopConAnalyzer.h"
-#include "CSCCablesHandler.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-typedef popcon::PopConAnalyzer<popcon::CSCCablesImpl> CSCCablesPopConAnalyzer;
-
-DEFINE_FWK_MODULE(CSCCablesPopConAnalyzer);
